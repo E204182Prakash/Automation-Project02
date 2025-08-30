46673c90de816433542f6bfaf8b84e85226c1f28
Prakash <E204182@esoft.academy>
2025-08-30 08:08:24 +0530

    Use environment variables for login credentials and URL

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+import os
+
 class LoginPage:
     def __init__(self, page):
         self.page = page
@@ -6,8 +9,11 @@ class LoginPage:
         self.login_button = "input[data-test='login-button']"
         self.error_message = "h3[data-test='error']"
 
+        load_dotenv(override=True)
+        self.url = os.getenv("SAUCE_URL")
+
     def load(self):
-        self.page.goto("https://www.saucedemo.com/")
+        self.page.goto(self.url)
 
     def login(self, username, password):
         self.page.fill(self.username_input, username)
--- Tests/test_login.py
+++ Tests/test_login.py
@@ -1,12 +1,20 @@
 from Pages.login_page import LoginPage
 from Pages.inventory_page import InventoryPage
+from dotenv import load_dotenv
+import os
+
+# Load .env and override OS vars
+load_dotenv(override=True)
+USERNAME = os.getenv("SAUCE_USERNAME")
+PASSWORD = os.getenv("SAUCE_PASSWORD")
 
 def test_valid_login(browser_page):
     login = LoginPage(browser_page)
     login.load()
-    login.login("standard_user", "secret_sauce")
+    login.login(USERNAME, PASSWORD)
     inventory = InventoryPage(browser_page)
-    assert inventory.is_loaded()
+    assert inventory.is_loaded(), "Inventory page did not load after login"
+    print("successfull login")
 
 def test_invalid_login(browser_page):
     login = LoginPage(browser_page)
