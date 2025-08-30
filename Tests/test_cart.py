e1e80cd88c556392e8f7b970233a7bc644c07b83
Prakash <E204182@esoft.academy>
2025-08-30 08:17:09 +0530

    Use environment variables for login credentials

--- Tests/test_cart.py
+++ Tests/test_cart.py
@@ -1,11 +1,17 @@
 from Pages.login_page import LoginPage
 from Pages.inventory_page import InventoryPage
 from Pages.cart_page import CartPage
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+USERNAME = os.getenv("SAUCE_USERNAME")
+PASSWORD = os.getenv("SAUCE_PASSWORD")
 
 def test_add_to_cart(browser_page):
     login = LoginPage(browser_page)
     login.load()
-    login.login("standard_user", "secret_sauce")
+    login.login(USERNAME, PASSWORD)
 
     inventory = InventoryPage(browser_page)
     inventory.add_to_cart()
